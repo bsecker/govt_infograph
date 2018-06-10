77f7133d3bcc4972692ec53736e3031c5c8885e2
benjamin <benjamin.secker@gmail.com>
2018-06-10 14:29:40 +1200

    made some components

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,3 +1,32 @@
-function question() {
-    this.
+// individual question with answer list
+function component(question, answerList) {
+    this.question = question;
+    this.answerList = answerList;
+    this.render = function () {
+        var outputString = "";
+
+        // add question
+        outputString.concat(this.question);
+
+        // add dropdown with answers
+
+        // let som = `<div>${myVar}</div>`;
+        return this.question;
+    };
 }
+
+componentList = [
+    component("I am asking about ", ['myself','another person']),
+    component("I need a ", ['minor operation','major operation', 'GP visit']),
+    component("I am after ", ['something specific','general info']),
+];
+
+// update visible content.
+function updateContent() {
+    var parsed = new DOMParser().parseFromString(componentList[current].render(), "text/html");
+    document.getElementById("content").innerHTML(parsed);
+    current++;
+
+}
+
+var current = 0;
